1d45f848d6b4e68c65c767c118f86b34a5aef0a2
Jeanne Moore <mooresea910@gmail.com>
2021-09-01 19:14:23 -0400

    Initial commit

---
+++ src/main/java/solution01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+/*
+ *     UCF   COP3330  Fall 2021 Assignment 1 Solution01
+ *     Copyright 2021 Sean Moore
+ */
+public class solution01 {
+    /*
+    Create a Java Application that
+     prompts the user for their name
+     takes a name as input
+     prints "Hello, <name>, nice to meet you!"
+     Uses string concatenation
+     */
+    //Challenge: Use no variables
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("What is your name? ");
+        String str;
+        str = input.nextLine();
+        System.out.println("Hello, ");
+        System.out.println(str);
+        System.out.println(", nice to meet you!");
+
+
+        //Challenge 1 print Statement
+        //replace 3 System outputs with this single line
+        // System.out.println("Hello, " + input.nextLine() + ", nice to meet you!");
+    }
+}
